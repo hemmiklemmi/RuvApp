7a4cd1fa1f5c9ee7d5b22213282bb498fa7f82de
hemmiklemmi <hemmioskar@gmail.com>
2021-11-05 18:39:07 +0000

    vann í ui.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // TODO importa því sem nota þarf
 //import { doc } from 'prettier';
 import { el } from './lib/helpers.js';
-import { fetchNews,cache} from './lib/news.js';
+import { fetchNews,cache } from './lib/news.js';
+import { fetchAndRenderCategory, fetchAndRenderLists } from './lib/ui.js';
 
 //const { getSupportInfo } = require('prettier');
 
@@ -9,7 +10,7 @@ import { fetchNews,cache} from './lib/news.js';
 const CATEGORY_ITEMS_ON_FRONTPAGE = 5;
 
 /** Vísun í <main> sem geymir allt efnið og við búum til element inn í */
-const main = document.querySelector('main');
+ const main = document.querySelector('main');
 
 /**
  * Athugar útfrá url (`window.location`) hvað skal birta:
@@ -17,35 +18,25 @@ const main = document.querySelector('main');
  * - `/?category=X` birtir yfirlit fyrir flokk `X`
  */
 async function route() {
-  main.classList.add("newsList__list")
-  let result = await fetchNews();
-  const h = document.createElement('section');
-  h.classList.add('news');
-  h.classList.add("newsList__item");
-  h.textContent= "hallo";
-  main.append(h);
-  //for(let i = 0; i < result.length; i++) {
-  //  main.append(el('section', result[i].title));
-  //}
-  console.log(result);
-  console.log(window.location);
+  const url =(window.location);
+  fetchAndRenderLists(main,5);
+
 }
 
-  // Athugum hvort það sé verið að biðja um category í URL, t.d.
-  // /?category=menning
+// Athugum hvort það sé verið að biðja um category í URL, t.d.
+// /?category=menning
 
-  // Ef svo er, birtum fréttir fyrir þann flokk
+// Ef svo er, birtum fréttir fyrir þann flokk
 
-  // Annars birtum við „forsíðu“
+// Annars birtum við „forsíðu“
 
 /**
  * Sér um að taka við `popstate` atburð sem gerist þegar ýtt er á back takka í
  * vafra. Sjáum þá um að birta réttan skjá.
  */
 window.onpopstate = () => {
-  console.log("pop");
+  console.log('pop');
 };
 
 // Í fyrsta skipti sem vefur er opnaður birtum við það sem beðið er um út frá URL
 route();
-
